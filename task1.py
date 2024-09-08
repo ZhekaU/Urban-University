d69f88b5ad74cdd324feca49728eb822142e29bb
ZhekaU <zekaushakov@yandex.ru>
2024-09-08 22:23:53 +0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+#"1st program"
+print(9**0.5*5)
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+#"3nd program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+#"4nd program"
+print('123.456')
+print((int((float('123.456'))*10)) %10)
+#"4nd program" я так вижу :)
+print('123.456')
+print ('123.456'[4])
+
