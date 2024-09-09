039e754a32c474b365e28c4afd76228ea8c58828
ZhekaU <zekaushakov@yandex.ru>
2024-09-09 18:39:31 +0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -2,4 +2,4 @@ completed_tasks=12
 hours_spent=1.5
 name_work='Python'
 Time_for_one_task= hours_spent/completed_tasks
-print(name_work,completed_tasks,hours_spent,Time_for_one_task)
+print("Курс:",name_work,",всего задач:",completed_tasks,",затрачено часов:",hours_spent,",""среднее время выполнения",Time_for_one_task,"часа.")
