4eead121006a550b46d57dc1ae6820443762c2c5
ZhekaU <zekaushakov@yandex.ru>
2024-09-11 10:09:52 +0700

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -5,7 +5,8 @@ my_dict['BMW']='Л 888 НМ 144'
 print(my_dict)
 my_dict.update({'Toyota':'Г 454 ЗХ 104', 'Geely':'В 843 ШП 77'})
 print(my_dict)
-del my_dict ['Mercedes'] #?
+trashcan_my_dict=my_dict.pop('Mercedes')
+print(trashcan_my_dict)
 
 print(my_dict)
 my_set={2,4,6,2,2,2,3,3,4,4,4,5,5,56,6,False,False,False,False,True,'Молоко'}
