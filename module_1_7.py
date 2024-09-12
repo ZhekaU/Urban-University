808ef8d3d977f62569feb19df1f76d5e2d50f56c
ZhekaU <zekaushakov@yandex.ru>
2024-09-12 22:25:32 +0700

    Update module_1_7.py

--- module_1_7.py
+++ module_1_7.py
@@ -1,11 +1,12 @@
 grades=[[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students={'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-grades[0]=sum(grades[0])/5
-print(grades)
+students_1=sorted(students) # Отсортировал в алфавитном порядке
+grades_1=sum(grades [0])/(len(grades[0]))
+grades_2=sum(grades [1])/(len(grades[1]))
+grades_3=sum(grades [2])/(len(grades[2]))
+grades_4=sum(grades [3])/(len(grades[3]))
+grades_5=sum(grades [4])/(len(grades[4]))
+average_rating=dict(zip(students_1, grades_1,grades_2,grades_3,grades_4,grades_5))
 
-#students_1 =tuple,sorted(students) #Множество преобразовали в кортеж и расположили по алфавиту
-#grades_1= tuple(grades)
-#average_rating= dict(zip(grades_1,students_1))
-
-#print(average_rating)
+print(average_rating)
 
