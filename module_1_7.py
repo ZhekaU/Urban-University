f13bc4e7bbcb02d0e8a40cb448fb2a9984193eda
ZhekaU <zekaushakov@yandex.ru>
2024-09-12 14:45:03 +0700

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,11 @@
+grades=[[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students={'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades[1]=sum(grades[1])/5
+print(grades)
+
+#students_1 =tuple,sorted(students) #Множество преобразовали в кортеж и расположили по алфавиту
+#grades_1= tuple(grades)
+#average_rating= dict(zip(grades_1,students_1))
+
+#print(average_rating)
+
