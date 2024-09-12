22f413a5a223fce255d421f4b027e1e81c817e62
ZhekaU <zekaushakov@yandex.ru>
2024-09-12 11:59:58 +0700

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -2,6 +2,6 @@ immutable_var=1,True,'Мама'
 print(immutable_var)
 #immutable_var[1]=False # Закоментил, чтобы не ругался 
 print(immutable_var) # Кортеж не работает с обращением на конкретный элемент и не изменяет его
-mutable_list=([1,True,'Мама'],2024)
-mutable_list[0][0]=2020
+mutable_list=[1,True,'Мама']
+mutable_list[0]=2020
 print(mutable_list)
