494514be8ba55b73894a2724f7940c993a76dcf4
ZhekaU <zekaushakov@yandex.ru>
2024-09-12 14:54:38 +0700

    Update module_1_7.py

--- module_1_7.py
+++ module_1_7.py
@@ -1,6 +1,6 @@
 grades=[[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students={'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-grades[1]=sum(grades[1])/5
+grades[0]=sum(grades[0])/5
 print(grades)
 
 #students_1 =tuple,sorted(students) #Множество преобразовали в кортеж и расположили по алфавиту
