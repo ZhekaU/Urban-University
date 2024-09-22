95b35f72c037df50ffec94f840a3609256f1c46e
ZhekaU <zekaushakov@yandex.ru>
2024-09-22 16:56:37 +0700

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,7 +4,7 @@ random_number_1 = random.choice(random_number) # Генерация случай
 print (f"Случайное число: {random_number_1}")
 pairs = [] #Поиск пар чисел
 for i in range(1, random_number_1):
-    for j in range(1, random_number_1):
+    for j in range(i + 1, random_number_1):
         if random_number_1 % (i + j) == 0:
             pairs.append((i, j))
 result = ''
