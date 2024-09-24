9b1b246b4c577bf172b18100d69bda3306c36422
ZhekaU <zekaushakov@yandex.ru>
2024-09-24 16:30:02 +0700

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -2,8 +2,10 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
     if sender == recipient:
         print('Нельзя отправить письмо самому себе!')
         return
-    if ('@' not in recipient and sender or not recipient.endswith
-            and sender.endswith((".com", ".ru", ".net"))):
+       if ('@' not in recipient or '@' not in sender or not recipient.endswith((".com", ".ru", ".net"))
+            or not sender.endswith((".com", ".ru", ".net"))):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    else:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     if sender == 'university.help@gmail.com':
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient} '
