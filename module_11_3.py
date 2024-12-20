87c0cdcb767caa5379bd8baa6c49bd92a018b0d6
ZhekaU <zekaushakov@yandex.ru>
2024-12-20 21:36:28 +0700

    Update module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -1,11 +1,13 @@
+from pprint import pprint
 def introspection_info(*obj):
     info = {
         'type': type(obj),
-        'attributes': dir(obj),
+        'attributes': [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith('__')],
         'methods': [method for method in dir(obj) if callable(getattr(obj, method))],
         'module': getattr(obj, '__module__', 'No module attribute'),
         'doc': getattr(obj, '__doc__', 'No docstring')
     }
     return info
-number_info = introspection_info(42,"time")
-print(number_info)
+
+number_info = introspection_info(42,'time')
+pprint(number_info)
