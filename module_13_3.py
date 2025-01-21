d792e37f1860f2676ff613e69d4117f8783ff6ec
ZhekaU <zekaushakov@yandex.ru>
2025-01-21 20:49:28 +0700

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,31 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = 'Ключ бота'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+
+
+@dp.message_handler(text = ['realty','bb'])
+async  def realty_message(message):
+    print('Realty message')
+
+
+@dp.message_handler(commands=['start'])
+async  def start_message(message):
+    print('start_message')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print('all_massages')
+    await message.answer('Введите команду /start, чтобы начать общение')
+
+
+@dp.message_handler()
+async  def all_message(message):
+    print('Новое сообщение!')
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
