e75aa994a291b41a7996cb29fdd0e37f7340ba8c
ZhekaU <zekaushakov@yandex.ru>
2025-02-04 21:35:39 +0700

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,41 @@
+import sqlite3
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS Users 
+(id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+
+)   ''')
+
+
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+cursor.execute('SELECT COUNT(*) FROM Users')
+record_count = cursor.fetchone()[0]
+if record_count == 0:
+    for i in range(1, 11):
+        cursor.execute('INSERT OR IGNORE INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i}', f'example{i}@gmail.com', 10 * i, 1000))
+    connection.commit()
+
+cursor.execute('SELECT id FROM Users')
+b = cursor.fetchall()
+for i, b in enumerate(b):
+    if i % 2 == 1:
+            cursor.execute ('UPDATE Users SET balance = 500  WHERE id = ?' ,(i,))
+
+cursor.execute('SELECT id FROM Users')
+rows = cursor.fetchall()
+for i in range(len(rows)+1):
+    if i % 3 == 1:
+        cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
+
+cursor.execute('SELECT username,email,age, balance FROM Users WHERE age != ?', (60,))
+users = cursor.fetchall()
+for user in users:
+    print(user)
+
+connection.commit()
+connection.close()
